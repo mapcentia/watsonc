105663aba8f77a4182f1064e6d0dd5fd4324d61f
Martin Høgh <mh@mapcentia.com>
2020-09-23 13:12:50 +0200

    New IP

--- browser/index.js
+++ browser/index.js
@@ -1075,7 +1075,6 @@ module.exports = module.exports = {
     },
 
     enableChemical(chemicalId, layersToEnable = [], onComplete = false) {
-        console.log("chemicalId first", chemicalId);
         if (!chemicalId) throw new Error(`Chemical identifier was not provided`);
         setTimeout(() => {
             let layersToEnableWereProvided = (layersToEnable.length > 0);
@@ -1098,8 +1097,6 @@ module.exports = module.exports = {
                     }
                 }
             }
-            console.log("chemicalId second", chemicalId);
-
             layerTree.applyFilters({
                 "system.all": {
                     match: "any", columns: [
