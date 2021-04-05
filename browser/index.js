5ccdb743871c3363c78f681cb85d13af412786c4
Aswin Murugesh <aswinmurugesh94@gmail.com>
2021-04-05 17:19:20 +0530

    Add option for theming

--- browser/index.js
+++ browser/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import {Provider} from 'react-redux';
+import ThemeProvider from "./themes/ThemeProvider";
+import LightTheme from "./themes/LightTheme";
+
 
 import PlotManager from './PlotManager';
 import ModalComponent from './components/ModalComponent';
@@ -847,7 +850,7 @@ module.exports = module.exports = {
         const introlModalPlaceholderId = `watsonc-intro-modal-placeholder`;
         if ($(`#${introlModalPlaceholderId}`).is(`:empty`)) {
             try {
-                ReactDOM.render(<Provider store={reduxStore}>
+                /* ReactDOM.render(<Provider store={reduxStore}>
                     <IntroModal
                         ref={inst => {
                             infoModalInstance = inst;
@@ -860,7 +863,10 @@ module.exports = module.exports = {
                         categories={categoriesOverall ? categoriesOverall : []}
                         onApply={_self.onApplyLayersAndChemical}
                         onClose={onCloseHandler}
-                    /></Provider>, document.getElementById(introlModalPlaceholderId));
+                    /></Provider>, document.getElementById(introlModalPlaceholderId)); */
+                ReactDOM.render(<ThemeProvider>
+                    <Modal />
+                    </ThemeProvider>, document.getElementById(introlModalPlaceholderId))
             } catch (e) {
                 console.error(e);
             }
---
+++ browser/themes/LightTheme.js
@@ -0,0 +1,9 @@
+export const LightTheme = {
+  colors: {
+    titleColor: '#19B037'
+  },
+  padding: {
+    titlePadding: '30px'
+  },
+  backgroundColor: '#ddd'
+}
