d0505153fafc1ec9d0229f13d55d93d8f1788618
Aswin Murugesh <aswinmurugesh94@gmail.com>
2021-04-14 11:46:01 +0530

    Close button component

--- browser/components/shared/dialogue/Dialogue.js
+++ browser/components/shared/dialogue/Dialogue.js
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 import Title from '../title/Title';
+import CloseButton from '../button/CloseButton';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
 
 function Dialogue(props) {
     return (
         <Root>
             <div className="modal-header">
-                <Title text={props.titleText} />
+                <Grid container>
+                    <Grid container item xs={10}>
+                        <Title text={props.titleText} />
+                    </Grid>
+                    <Grid container justify="flex-end" item xs={2}>
+                        <CloseButton onClick={props.onCloseButtonClick} />
+                    </Grid>
+                </Grid>
             </div>
             <div className="modal-body">
                 {props.children}
@@ -19,6 +28,8 @@ Button.propTypes = {
 }
 
 const Root = styled.div`
-    background: ${({ theme }) => theme.colors.background};
+    background: ${({ theme }) => theme.colors.primary[1]};
+    padding: 24px 32px 24px 32px;
+    border-radius: ${({ theme }) => `${theme.layout.borderRadius.large}px`};
 `
 export default Dialogue;
