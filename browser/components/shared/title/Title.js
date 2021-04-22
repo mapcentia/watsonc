488537be7090d780d8a78d34026af3efe4b73d8f
Aswin Murugesh <aswinmurugesh94@gmail.com>
2021-04-22 16:53:20 +0530

    PR comments

--- browser/components/shared/title/Title.js
+++ browser/components/shared/title/Title.js
@@ -1,19 +1,64 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import PropTypes from 'prop-types';
 
 function Title(props) {
     return (
-        <Root>
+        <Root level={props.level}>
             {props.text}
         </Root>
     );
 }
 Title.propTypes = {
-    text: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    level: PropTypes.number,
+    className: PropTypes.string,
+    align: PropTypes.string,
+}
+
+Title.defaultProps = {
+    level: 1,
+    align: 'left'
+
 }
 
 const Root = styled.h4`
-    color: ${props => props.theme.colors.headings};
-    font: ${props => props.theme.fonts.title};
+    display: "block",
+    fontWeight: "normal",
+    margin: 0,
+    lineHeight: 1.3
+    text-align: ${props => props.align};
+    ${({ level, theme }) => {
+        const styles = {
+
+            1: css `
+                font: ${props => props.theme.fonts.title};
+                color: ${props => props.theme.colors.headings};
+            `,
+            2: css `
+                font: ${props => props.theme.fonts.subtitle};
+            `,
+            3: css `
+                font: ${props => props.theme.fonts.heading};
+                color: ${props => props.theme.colors.gray[3]};
+            `,
+            4: css `
+                font: ${props => props.theme.fonts.body};
+            `,
+            5: css `
+                margin-top: ${props => props.theme.layout.gutter/2}px;
+                font: ${props => props.theme.fonts.subbody};
+                color: ${props => props.theme.colors.gray[4]};
+            `,
+            6: css `
+                margin-top: ${props => props.theme.layout.gutter/4}px;
+                font: ${props => props.theme.fonts.label};
+                color: ${props => props.theme.colors.gray[5]};
+            `,
+            7: css `
+                font: ${props => props.theme.fonts.footnote};
+            `,
+        }
+        return styles[level];
+    }}
 `
 export default Title;
