ed38bafeea5712c5636cc98c9f1d7a4dd3ecf82a
Aswin Murugesh <aswinmurugesh94@gmail.com>
2021-05-05 13:43:17 +0530

    Project list styling

--- shared/icons/icons.ts
+++ shared/icons/icons.ts
@@ -1,6 +1,7 @@
 export const IconName = {
   CHECK_MARK_SOLID: "check-mark-solid" as "check-mark-solid",
   CLEANING_SPRAY: "cleaning-spray" as "cleaning-spray",
+  DASHBOARD: "dashboard" as "dashboard",
   HYPERLINK: "hyperlink" as "hyperlink",
   LAB_FLASK_EXPERIMENT: "lab-flask-experiment" as "lab-flask-experiment",
   NO3_SOLID: "no3-solid" as "no3-solid",
