541c7307cb92f54301fe97191b920cb931b9a897
Aswin Murugesh <aswinmurugesh94@gmail.com>
2021-05-10 13:03:23 +0530

    Create ProjectListItem component

--- browser/components/dataselector/ProjectList.js
+++ browser/components/dataselector/ProjectList.js
@@ -1,25 +1,18 @@
 import { useState, useEffect } from 'react';
 import {connect} from 'react-redux'
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import ProjectsApi from '../../api/projects/ProjectsApi';
-import Card from '../shared/card/Card';
 import Title from '../shared/title/Title';
 import styled from "styled-components";
 import Grid from '@material-ui/core/Grid';
 import {DarkTheme} from '../../themes/DarkTheme';
-import {Spacing} from '../shared/constants/spacing';
 import {Align} from '../shared/constants/align';
 import Button from '../shared/button/Button';
-import Icon from '../shared/icons/Icon';
-import {Variants} from '../shared/constants/variants';
+import ProjectListItem from './ProjectListItem';
 
 
 function ProjectList(props) {
     const [projects, setProjects] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [disableStateApply, setDisableStateApply] = useState(false);
     const [hoveredItem, setHoveredItem] = useState();
 
     const loadProjects = () => {
@@ -32,63 +25,6 @@ function ProjectList(props) {
                 setIsLoading(false);
             });
         })
-    }
-
-    const applySnapshot = (project) => {
-        if (disableStateApply) {
-            return;
-        }
-        console.log(project)
-        if (props.onStateSnapshotApply) props.onStateSnapshotApply();
-        setDisableStateApply(true);
-        props.state.applyState(project.snapshot).then(() => {
-            setDisableStateApply(false);
-        });
-    }
-
-    const setHoverItem = (project) => {
-        setHoveredItem(project.id);
-    }
-
-    const removeHoverItem = (event) => {
-        setHoveredItem(null);
-    }
-
-    const getProjectParameters = (project) => {
-        let parameters = [];
-        let queryParameters = props.urlparser.urlVars;
-        parameters.push(`state=${project.id}`);
-        let highPriorityConfigString = false, lowPriorityConfigString = false;
-        if (`config` in queryParameters && queryParameters.config) {
-            lowPriorityConfigString = queryParameters.config;
-        }
-
-        if (project.snapshot && project.snapshot.meta) {
-            if (project.snapshot.meta.config) {
-                highPriorityConfigString = project.snapshot.meta.config;
-            }
-
-            if (project.snapshot.meta.tmpl) {
-                parameters.push(`tmpl=${project.snapshot.meta.tmpl}`);
-            }
-        }
-
-        let configParameter = ``;
-        if (highPriorityConfigString) {
-            configParameter = `config=${highPriorityConfigString}`;
-            parameters.push(configParameter);
-        } else if (lowPriorityConfigString) {
-            configParameter = `config=${lowPriorityConfigString}`;
-            parameters.push(configParameter);
-        }
-        return parameters;
-
-    }
-
-    const getPermalinkForProject = (project) => {
-        let parameters = getProjectParameters(project);
-        let permalink = `${window.location.origin}${props.anchor.getUri()}?${parameters.join('&')}`;
-        return permalink;
     }
 
     useEffect(() => {
@@ -105,22 +41,8 @@ function ProjectList(props) {
                         <Grid container>
                             <Grid container item xs={12}>
                                 {projects.map((project, index) => {
-                                    return (<Card key={index} spacing={Spacing.Lite}  onClick={() => applySnapshot(project)} onMouseEnter={() => setHoverItem(project)} onMouseLeave={removeHoverItem}>
-                                        <Grid container>
-                                            <Grid container item md={7}>
-                                                <Icon name='dashboard' variant={Variants.Primary} strokeColor={hoveredItem == project.id ? DarkTheme.colors.primary[2] : DarkTheme.colors.primary[4]} size={24} marginRight={8} />
-                                                <Title text={project.title} level={4} color={DarkTheme.colors.headings} />
-                                            </Grid>
-                                            <Grid container item md={5} justify='flex-end'>
-
-                                                <IconContainer onClick={e => e.stopPropagation()}>
-                                                    <CopyToClipboard text={getPermalinkForProject(project)} onCopy={() => toast.warning("Copied", { autoClose: 2000 })}>
-                                                        <Icon name='hyperlink' variant={Variants.Primary} size={12} strokeColor={DarkTheme.colors.headings} />
-                                                    </CopyToClipboard>
-                                                </IconContainer>
-                                            </Grid>
-                                        </Grid>
-                                    </Card>)}
+                                        return <ProjectListItem project={project} {...props} key={index} />
+                                    }
                                 )}
                             </Grid>
                         </Grid>
@@ -135,19 +57,6 @@ function ProjectList(props) {
 const Root = styled.div`
     margin-top: ${props => props.theme.layout.gutter/2}px;
     margin-bottom: ${props => props.theme.layout.gutter*2}px;
-`;
-
-const IconContainer = styled.div`
-    display: inline-block;
-    width: ${props => props.theme.layout.gutter * 3/4}px;
-    height: ${props => props.theme.layout.gutter * 3/4}px;
-    cursor: pointer;
-    padding: 3px;
-    padding-left: 5px;
-    &:hover {
-        border: 1px solid ${props => props.theme.colors.primary[2]};
-        border-radius: ${props => props.theme.layout.borderRadius.small}px;
-    }
 `;
 
 const mapStateToProps = state => ({
--- browser/components/shared/card/Card.js
+++ browser/components/shared/card/Card.js
@@ -2,40 +2,10 @@ import styled, { css } from "styled-components";
 import PropTypes from 'prop-types';
 import { Spacing } from '../constants/spacing';
 
-Card.propTypes = {
-    spacing: PropTypes.string,
-    onClick: PropTypes.func,
-    onMouseEnter: PropTypes.func,
-    onMouseLeave: PropTypes.func,
-}
-
-Card.defaultProps = {
-    spacing: Spacing.Standard
-}
-
 function Card(props) {
 
-    const onClick = (event) => {
-        if (props.onClick) {
-            props.onClick(event);
-        }
-    }
-
-    const onMouseEnter = (event) => {
-        if (props.onMouseEnter) {
-            props.onMouseEnter(event);
-        }
-    }
-
-    const onMouseLeave = (event) => {
-        if (props.onMouseLeave) {
-            props.onMouseLeave(event);
-        }
-    }
-
     return (
-        <Root spacing={props.spacing} hasClick={!!props.onClick} onClick={onClick}
-            onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+        <Root>
             {props.children}
         </Root>
     );
@@ -47,24 +17,8 @@ const Root = styled.div`
     width: 100%;
     border: 0;
     box-shadow: none;
-    cursor: ${props => props.hasClick ? 'pointer' : 'inherit'};
-    &:hover {
-        background: ${props => props.hasClick ? props.theme.colors.primary[5] : props.theme.colors.primary[2]};
-    }
-    ${({ spacing, theme }) => {
-        const styles = {
-            [Spacing.Standard]: css `
-                margin-top: ${props => props.theme.layout.gutter}px;
-                padding: ${props => props.theme.layout.gutter/2}px;
-
-            `,
-            [Spacing.Lite]: css `
-                margin-top: ${props => props.theme.layout.gutter/2}px;
-                padding: ${props => props.theme.layout.gutter/4}px;
-            `
-        }
-        return styles[spacing];
-    }}
+    margin-top: ${props => props.theme.layout.gutter}px;
+    padding: ${props => props.theme.layout.gutter/2}px;
 `;
 
 export default Card;
