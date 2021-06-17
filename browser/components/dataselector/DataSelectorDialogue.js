9edd2dd1a4ca36495cb3081e081bb1740535db84
Martin Høgh <mh@mapcentia.com>
2021-06-17 10:37:29 +0200

    The two buttons "Pesticider" and "Nitrat" is now active. The last two are removed for now

--- browser/components/dataselector/DataSelectorDialogue.js
+++ browser/components/dataselector/DataSelectorDialogue.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import styled from "styled-components";
 import Title from '../shared/title/Title';
 import CloseButton from '../shared/button/CloseButton';
@@ -11,10 +11,10 @@ import Button from '../shared/button/Button';
 import ButtonGroup from '../shared/button/ButtonGroup';
 import ProjectList from './ProjectList';
 import PredefinedDatasourceViews from './PredefinedDatasourceViews';
-import { Variants } from '../shared/constants/variants';
-import { Size } from '../shared/constants/size';
-import { Align } from '../shared/constants/align';
-import { hexToRgbA } from '../../helpers/colors';
+import {Variants} from '../shared/constants/variants';
+import {Size} from '../shared/constants/size';
+import {Align} from '../shared/constants/align';
+import {hexToRgbA} from '../../helpers/colors';
 import {WATER_LEVEL_KEY} from '../../constants';
 import MetaApi from '../../api/meta/MetaApi';
 import Searchbox from '../shared/inputs/Searchbox';
@@ -57,6 +57,13 @@ function DataSelectorDialogue(props) {
             setDataSources(response);
         });
     }
+    const applyLayer = (layer, chem = false) => {
+        props.onApply({
+            layers: [layer],
+            chemical: chem
+        });
+        props.onCloseButtonClick()
+    }
 
     useEffect(() => {
         loadDataSources();
@@ -84,62 +91,68 @@ function DataSelectorDialogue(props) {
             <ModalHeader>
                 <Grid container>
                     <Grid container item xs={10}>
-                        <Title text={props.titleText} />
+                        <Title text={props.titleText}/>
                     </Grid>
                     <Grid container justify="flex-end" item xs={2}>
-                        <CloseButton onClick={props.onCloseButtonClick} />
+                        <CloseButton onClick={props.onCloseButtonClick}/>
                     </Grid>
                 </Grid>
             </ModalHeader>
             <ModalBody>
                 {showProjectsList ? <ProjectList onStateSnapshotApply={props.onCloseButtonClick} {...props} /> :
                     <div>
-                        <PredefinedDatasourceViews />
+                        <PredefinedDatasourceViews applyLayer={applyLayer}/>
                         <GridContainer>
                             <Grid container spacing={32}>
                                 <Grid container item md={6}>
                                     <Card>
-                                        <Title text={__('Datakilder')} level={3} />
-                                        <CheckBoxList listItems={dataSources} onChange={setSelectedDataSources} selectedItems={selectedDataSources} />
+                                        <Title text={__('Datakilder')} level={3}/>
+                                        <CheckBoxList listItems={dataSources} onChange={setSelectedDataSources}
+                                                      selectedItems={selectedDataSources}/>
                                     </Card>
                                 </Grid>
                                 <Grid container item md={6}>
                                     {selectedDataSources.findIndex((item) => item.value == 'v:system.all') > -1 ? <Card>
-                                        <Searchbox placeholder={__('Søg efter måleparameter')} onChange={(value) => setParameterSearchTerm(value)} />
-                                        <RadioButtonList listItems={parameters} onChange={setSelectedParameter} selectedParameter={selectedParameter} />
+                                        <Searchbox placeholder={__('Søg efter måleparameter')}
+                                                   onChange={(value) => setParameterSearchTerm(value)}/>
+                                        <RadioButtonList listItems={parameters} onChange={setSelectedParameter}
+                                                         selectedParameter={selectedParameter}/>
                                     </Card> : null}
                                 </Grid>
                             </Grid>
                         </GridContainer>
                     </div>}
                 {showProjectsList ? <ButtonGroup align={Align.Center}>
-                    <Button text={__("Choose datasource and layers")} variant={Variants.None} onClick={() => setShowProjectsList(false)} size={Size.Large} />
+                        <Button text={__("Choose datasource and layers")} variant={Variants.None}
+                                onClick={() => setShowProjectsList(false)} size={Size.Large}/>
 
-                </ButtonGroup> :
-                <ButtonGroup align={Align.Center} spacing={2}>
-                    <Button text={__("Abn eksisterende")} variant={Variants.None} onClick={() => setShowProjectsList(!showProjectsList)} size={Size.Large} />
-                    <Button text={__("Start")} variant={Variants.Primary} onClick={() => applyParameter()} size={Size.Large} disabled={selectedDataSources.length === 0} />
-                </ButtonGroup> }
+                    </ButtonGroup> :
+                    <ButtonGroup align={Align.Center} spacing={2}>
+                        <Button text={__("Abn eksisterende")} variant={Variants.None}
+                                onClick={() => setShowProjectsList(!showProjectsList)} size={Size.Large}/>
+                        <Button text={__("Start")} variant={Variants.Primary} onClick={() => applyParameter()}
+                                size={Size.Large} disabled={selectedDataSources.length === 0}/>
+                    </ButtonGroup>}
             </ModalBody>
         </Root>
     );
 }
 
 const Root = styled.div`
-    background: ${({ theme }) => hexToRgbA(theme.colors.primary[1], 0.92)};
-    border-radius: ${({ theme }) => `${theme.layout.borderRadius.large}px`};
+  background: ${({theme}) => hexToRgbA(theme.colors.primary[1], 0.92)};
+  border-radius: ${({theme}) => `${theme.layout.borderRadius.large}px`};
 `;
 
 const ModalHeader = styled.div`
-    padding: ${( { theme }) => `${theme.layout.gutter}px ${theme.layout.gutter}px 0 ${theme.layout.gutter}px`};
+  padding: ${({theme}) => `${theme.layout.gutter}px ${theme.layout.gutter}px 0 ${theme.layout.gutter}px`};
 `;
 
 const ModalBody = styled.div`
-    padding: ${( { theme }) => `${theme.layout.gutter}px`};
+  padding: ${({theme}) => `${theme.layout.gutter}px`};
 `;
 
 const GridContainer = styled.div`
-    padding-top: ${props => props.theme.layout.gutter/2}px;
+  padding-top: ${props => props.theme.layout.gutter / 2}px;
 `;
 
 export default DataSelectorDialogue;
