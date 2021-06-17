22b4d0a1cdb429ba29ebfc9701eff93550e35fa0
Martin Høgh <mh@mapcentia.com>
2021-06-17 10:37:29 +0200

    The two buttons "Pesticider" and "Nitrat" is now active. The last two are removed for now

--- browser/components/dataselector/PredefinedDatasourceViews.js
+++ browser/components/dataselector/PredefinedDatasourceViews.js
@@ -1,13 +1,16 @@
 import IconButton from '../shared/button/IconButton';
+import {LAYER_NAMES} from '../../constants';
 
 function PredefinedDatasourceViews(props) {
 
+
     return (<>
-            <IconButton icon="cleaning-spray" label={__('Pesticider')} />
-            <IconButton icon="no3-solid" label={__('Nitrat')} />
-            <IconButton icon="water-wifi-solid" label={__('Mine stationer')} />
-            <IconButton icon="lab-flask-experiment" label={__('Mine favoritter')} />
-        </>);
+        <IconButton icon="cleaning-spray" label={__('Pesticider')}
+                    onClick={() => props.applyLayer("calypso_stationer.pesticidoverblik")}/>
+        <IconButton icon="no3-solid" label={__('Nitrat')} onClick={()=>props.applyLayer(LAYER_NAMES[0], "1176")}/>
+        {/*<IconButton icon="water-wifi-solid" label={__('Mine stationer')}/>*/}
+        {/*<IconButton icon="lab-flask-experiment" label={__('Mine favoritter')}/>*/}
+    </>);
 
 }
 
