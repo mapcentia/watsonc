3da35e76ba0180d85b0122a6d6a1fd4bcce766c2
Martin Høgh <mh@mapcentia.com>
2021-09-20 11:14:48 +0200

    Add empty onclick to avoid warning

--- browser/components/dashboardshell/GraphCard.js
+++ browser/components/dashboardshell/GraphCard.js
@@ -79,83 +79,83 @@ function GraphCard(props) {
 
     return (<li className='list-group-item'><Root>
         <DashboardPlotHeader>
-                <Grid container>
-                    <Grid container item xs={3}>
-                        <HeaderActionItem>
-                            <HeaderSvg>
-                                <Icon name='analytics-board-graph-line' size={16} />
-                            </HeaderSvg>
-                            <Title level={5} text={props.plot.title} marginLeft={4} />
-                        </HeaderActionItem>
-                    </Grid>
-                    <Grid container item xs={2}>
-                        <Button variant={Variants.Primary} size={Size.Small} text={__('Gem')} />
-                    </Grid>
-                    <Grid container item xs={7} justify='flex-end'>
-                        {props.cardType === 'plot' ? <HeaderActionItem onClick={download}>
-                            <IconContainer>
-                                <Icon name="arrow-down" size={16} />
-                            </IconContainer>
-                            <Title marginLeft={8} level={6} text={__('Download')} />
-                        </HeaderActionItem> : null}
-                        <SortHandleComponent />
-                        <CloseButton onClick={props.onRemove}>
-                            <Icon name="cross" size={24} />
-                        </CloseButton>
-                    </Grid>
+            <Grid container>
+                <Grid container item xs={3}>
+                    <HeaderActionItem>
+                        <HeaderSvg>
+                            <Icon name='analytics-board-graph-line' size={16}/>
+                        </HeaderSvg>
+                        <Title level={5} text={props.plot.title} marginLeft={4}/>
+                    </HeaderActionItem>
                 </Grid>
-            </DashboardPlotHeader>
-            {props.cardType === 'plot' ? <DashboardPlotCard {...props} /> : <DashboardProfileCard {...props} />}
-        </Root></li>);
+                <Grid container item xs={2}>
+                    <Button variant={Variants.Primary} onClick={() => {}} size={Size.Small} text={__('Gem')}/>
+                </Grid>
+                <Grid container item xs={7} justify='flex-end'>
+                    {props.cardType === 'plot' ? <HeaderActionItem onClick={download}>
+                        <IconContainer>
+                            <Icon name="arrow-down" size={16}/>
+                        </IconContainer>
+                        <Title marginLeft={8} level={6} text={__('Download')}/>
+                    </HeaderActionItem> : null}
+                    <SortHandleComponent/>
+                    <CloseButton onClick={props.onRemove}>
+                        <Icon name="cross" size={24}/>
+                    </CloseButton>
+                </Grid>
+            </Grid>
+        </DashboardPlotHeader>
+        {props.cardType === 'plot' ? <DashboardPlotCard {...props} /> : <DashboardProfileCard {...props} />}
+    </Root></li>);
 }
 
 
 const Root = styled.div`
-    background: ${props => props.theme.colors.gray[5]};
-    width: 100%;
-    margin-top: ${props => props.theme.layout.gutter/4}px;
-    border-radius: ${props => props.theme.layout.borderRadius.medium}px;
-    height: ${props => props.theme.layout.gutter*12.5}px;
+  background: ${props => props.theme.colors.gray[5]};
+  width: 100%;
+  margin-top: ${props => props.theme.layout.gutter / 4}px;
+  border-radius: ${props => props.theme.layout.borderRadius.medium}px;
+  height: ${props => props.theme.layout.gutter * 12.5}px;
 `;
 
 const DashboardPlotHeader = styled.div`
-    background: ${props => props.theme.colors.headings};
-    height: ${props => props.theme.layout.gutter*1.5}px;
-    width: 100%;
-    color: ${props => props.theme.colors.primary[2]};
-    padding: ${props => props.theme.layout.gutter*3/8}px ${props => props.theme.layout.gutter/2}px;
-    border-radius: ${props => props.theme.layout.borderRadius.medium}px;
+  background: ${props => props.theme.colors.headings};
+  height: ${props => props.theme.layout.gutter * 1.5}px;
+  width: 100%;
+  color: ${props => props.theme.colors.primary[2]};
+  padding: ${props => props.theme.layout.gutter * 3 / 8}px ${props => props.theme.layout.gutter / 2}px;
+  border-radius: ${props => props.theme.layout.borderRadius.medium}px;
 `;
 
 const HeaderSvg = styled.div`
-    display: inline-block;
-    padding: ${props => props.theme.layout.gutter/8}px;
-    vertical-align: middle;
+  display: inline-block;
+  padding: ${props => props.theme.layout.gutter / 8}px;
+  vertical-align: middle;
 `;
 
 const HeaderActionItem = styled.div`
-    margin-right: ${props => props.theme.layout.gutter/2}px;
-    vertical-align: middle;
-    cursor: pointer;
+  margin-right: ${props => props.theme.layout.gutter / 2}px;
+  vertical-align: middle;
+  cursor: pointer;
 `;
 
 
 const IconContainer = styled.div`
-    height: ${props => props.theme.layout.gutter*3/4}px;
-    width: ${props => props.theme.layout.gutter*3/4}px;
-    background: ${props => props.theme.colors.gray[4]};
-    display: inline-block;
-    border-radius: 50%;
-    padding: ${props => props.theme.layout.gutter/8}px;
-    vertical-align: middle;
+  height: ${props => props.theme.layout.gutter * 3 / 4}px;
+  width: ${props => props.theme.layout.gutter * 3 / 4}px;
+  background: ${props => props.theme.colors.gray[4]};
+  display: inline-block;
+  border-radius: 50%;
+  padding: ${props => props.theme.layout.gutter / 8}px;
+  vertical-align: middle;
 `;
 
 const CloseButton = styled.div`
-    display: inline-block;
-    border-radius: ${props => props.theme.layout.borderRadius.small}px;
-    border: 1px solid ${props => props.theme.colors.gray[4]};
-    height: ${props => props.theme.layout.gutter * 3 / 4}px;
-    cursor: pointer;
+  display: inline-block;
+  border-radius: ${props => props.theme.layout.borderRadius.small}px;
+  border: 1px solid ${props => props.theme.colors.gray[4]};
+  height: ${props => props.theme.layout.gutter * 3 / 4}px;
+  cursor: pointer;
 `;
 
 export default sortableElement(GraphCard);
