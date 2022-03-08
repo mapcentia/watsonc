2a099241c76f52ad32f919172fe2c88cd4622b82
Kolopiolo <mb@watsonc.dk>
2022-03-08 09:36:12 +0100

    Update PlotComponent.js
    Added yaxis logic
    Added custom component for autoscaling
    Added custom_colorscale for specified graphs
    Refactored PlotComponent to be more "react" like

--- browser/components/dashboardshell/PlotComponent.js
+++ browser/components/dashboardshell/PlotComponent.js
@@ -1,4 +1,5 @@
 import Plot from "react-plotly.js";
+// import Plotly from "plotly.js";
 import { useState, useEffect } from "react";
 import moment from "moment";
 // import { Colorscales } from "../shared/constants/colorscales";
@@ -13,25 +14,69 @@ function next_color() {
   };
 }
 
+var autoscale = {
+  width: 500,
+  //   height: "1em",
+  //   viewBox: "0 0 1000 1000",
+  path: "M447.1 319.1v135.1c0 13.26-10.75 23.1-23.1 23.1h-135.1c-12.94 0-24.61-7.781-29.56-19.75c-4.906-11.1-2.203-25.72 6.937-34.87l30.06-30.06L224 323.9l-71.43 71.44l30.06 30.06c9.156 9.156 11.91 22.91 6.937 34.87C184.6 472.2 172.9 479.1 160 479.1H24c-13.25 0-23.1-10.74-23.1-23.1v-135.1c0-12.94 7.781-24.61 19.75-29.56C23.72 288.8 27.88 288 32 288c8.312 0 16.5 3.242 22.63 9.367l30.06 30.06l71.44-71.44L84.69 184.6L54.63 214.6c-9.156 9.156-22.91 11.91-34.87 6.937C7.798 216.6 .0013 204.9 .0013 191.1v-135.1c0-13.26 10.75-23.1 23.1-23.1h135.1c12.94 0 24.61 7.781 29.56 19.75C191.2 55.72 191.1 59.87 191.1 63.1c0 8.312-3.237 16.5-9.362 22.63L152.6 116.7l71.44 71.44l71.43-71.44l-30.06-30.06c-9.156-9.156-11.91-22.91-6.937-34.87c4.937-11.95 16.62-19.75 29.56-19.75h135.1c13.26 0 23.1 10.75 23.1 23.1v135.1c0 12.94-7.781 24.61-19.75 29.56c-11.1 4.906-25.72 2.203-34.87-6.937l-30.06-30.06l-71.43 71.43l71.44 71.44l30.06-30.06c9.156-9.156 22.91-11.91 34.87-6.937C440.2 295.4 447.1 307.1 447.1 319.1z",
+  // path: "M224 376V512H24C10.7 512 0 501.3 0 488v-464c0-13.3 10.7-24 24-24h336c13.3 0 24 10.7 24 24V352H248c-13.2 0-24 10.8-24 24zm76.45-211.36-96.42-95.7c-6.65-6.61-17.39-6.61-24.04 0l-96.42 95.7C73.42 174.71 80.54 192 94.82 192H160v80c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-80h65.18c14.28 0 21.4-17.29 11.27-27.36zM377 407 279.1 505c-4.5 4.5-10.6 7-17 7H256v-128h128v6.1c0 6.3-2.5 12.4-7 16.9z",
+  ascent: 500,
+  descent: -50,
+  //   transform: "matrix(1 0 0 -1 0 850)",
+};
+
 function PlotComponent(props) {
   const [plot, setPlot] = useState(
     <p className="text-muted">{__(`At least one y axis has to be provided`)}</p>
   );
-  let layout =
+  const [layoutState, setLayoutState] = useState(
     JSON.parse(JSON.stringify(config?.extensionConfig?.watsonc?.plotLayout)) ||
-    {};
+      {}
+  );
+  const [plotDataState, setPlotDataState] = useState(props.plotData);
+  const [configState, setConfigState] = useState({});
+  const [minmaxRange, setminmaxRange] = useState([0, 1]);
+  //   let layout =
+
+  const changeLayout = (minmax) => {
+    //   console.log(gd.layout);
+
+    return () =>
+      setLayoutState((prev) => {
+        prev.xaxis = {
+          ...prev.xaxis,
+          autorange: false,
+          range: [minmax[0], minmax[1]],
+        };
+        prev.yaxis = {
+          ...prev.yaxis,
+          autorange: true,
+        };
+        prev.yaxis2 = {
+          ...prev.yaxis2,
+          autorange: true,
+        };
+        prev.yaxis3 = {
+          ...prev.yaxis3,
+          autorange: true,
+        };
+        console.log(minmax);
+        console.log(prev);
+        return { ...prev };
+      });
+  };
+
   let yaxisTitles =
     JSON.parse(JSON.stringify(config?.extensionConfig?.watsonc?.yaxisTitles)) ||
     {};
 
   useEffect(() => {
     console.log(props.plotMeta);
+    let layout = JSON.parse(
+      JSON.stringify(config?.extensionConfig?.watsonc?.plotLayout)
+    );
 
-    let num_types = [
-      ...new Set(
-        props.plotData.map((elem) => elem.tstype_id).filter((elem) => elem != 4)
-      ),
-    ];
+    let num_types = [...new Set(props.plotData.map((elem) => elem.tstype_id))];
     console.log(num_types);
 
     const give_color = next_color();
@@ -63,9 +108,40 @@ function PlotComponent(props) {
         plotData = plotData.map((elem) => {
           return {
             ...elem,
-            yaxis: elem.tstype_id === num_types[0] ? "y1" : "y2",
+            yaxis: elem.tstype_id === num_types[0] ? "y1" : "y3",
           };
         });
+
+        layout.yaxis = {
+          ...layout.yaxis,
+          title: {
+            ...layout.yaxis.title,
+            text: yaxisTitles[num_types[0]],
+          },
+        };
+        layout.yaxis3 = {
+          ...layout.yaxis3,
+          title: {
+            ...layout.yaxis3.title,
+            text: yaxisTitles[num_types[1]],
+          },
+        };
+      } else if (num_types.length > 2) {
+        plotData = plotData.map((elem) => {
+          return {
+            ...elem,
+            yaxis:
+              elem.tstype_id === num_types[0]
+                ? "y1"
+                : elem.tstype_id === num_types[1]
+                ? "y2"
+                : "y3",
+          };
+        });
+        layout.xaxis = {
+          ...layout.xaxis,
+          domain: [0, 0.9],
+        };
         layout.yaxis = {
           ...layout.yaxis,
           title: {
@@ -79,50 +155,60 @@ function PlotComponent(props) {
             ...layout.yaxis2.title,
             text: yaxisTitles[num_types[1]],
           },
+          position: 0.9,
+          anchor: "free",
+        };
+        layout.yaxis3 = {
+          ...layout.yaxis3,
+          title: {
+            ...layout.yaxis3.title,
+            text: num_types.length == 3 ? yaxisTitles[num_types[2]] : "Øvrige",
+          },
+          position: 0.97,
+          anchor: "free",
         };
       }
     }
 
     if (plotData.length > 0) {
-      let min = moment.min(plotData.map((elem) => moment(elem.x[0])));
-      console.log(min);
+      let xmin = moment
+        .min(plotData.map((elem) => moment(elem.x[0])))
+        .format("YYYY-MM-DD HH:mm:ss.SSS");
 
-      let max = moment.max(plotData.map((elem) => moment(elem.x.slice(-1)[0])));
+      let xmax = moment
+        .max(plotData.map((elem) => moment(elem.x.slice(-1)[0])))
+        .format("YYYY-MM-DD HH:mm:ss.SSS");
+
+      setminmaxRange([xmin, xmax]);
+
       layout.xaxis = {
         ...layout.xaxis,
-        range: [min.valueOf(), max.valueOf()],
+        range: [xmin, xmax],
       };
 
       var resetAxes = {
-        name: "myResetScale2d",
-        title: "Reset axes",
-        click: function (gd) {
-          console.log(gd.layout);
-        },
+        name: "myAutoscale",
+        title: "Autoscale",
+        icon: autoscale,
+        click: changeLayout([xmin, xmax]),
       };
 
-      var config = {
+      var conf = {
+        modeBarButtons: [
+          ["toImage", "zoom2d", "pan2d", "zoomIn2d", "zoomOut2d"],
+          [resetAxes],
+          ["hoverClosestCartesian", "hoverCompareCartesian"],
+        ],
         // modeBarButtonsToAdd: [resetAxes],
-        // modeBarButtonsToRemove: ["resetScale2d"],
-        doubleClick: "autosize",
+        // modeBarButtonsToRemove: ["resetScale2d", "autoScale2d", "toggleSpikelines"],
+        doubleClick: false,
       };
     }
 
-    setPlot(
-      <Plot
-        data={plotData}
-        layout={layout}
-        config={config}
-        onLegendDoubleClick={(param) =>
-          console.log("Legend double clicked", param)
-        }
-        onLegendClick={(param) => console.log("Legend clicked", param)}
-        style={{ width: "100%", height: `100%` }}
-      />
-    );
-    if (props.yAxis2LayoutSettings) {
-      layout.yaxis2 = props.yAxis2LayoutSettings;
-    }
+    console.log(layout);
+    setConfigState(conf);
+    setLayoutState(layout);
+    setPlotDataState(plotData);
   }, [props.plotData, props.yAxis2LayoutSettings]);
 
   return (
@@ -133,7 +219,17 @@ function PlotComponent(props) {
           border: `1px solid lightgray`,
         }}
       >
-        {plot}
+        <Plot
+          data={plotDataState}
+          layout={layoutState}
+          config={configState}
+          onLegendDoubleClick={(param) =>
+            console.log("Legend double clicked", param)
+          }
+          onDoubleClick={changeLayout(minmaxRange)}
+          onLegendClick={(param) => console.log("Legend clicked", param)}
+          style={{ width: "100%", height: `100%` }}
+        />
       </div>
     </div>
   );
