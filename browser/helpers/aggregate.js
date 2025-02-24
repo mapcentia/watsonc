3000244a8feb656bd3d579086d0a7a28cc32cf3b
MathiasBuus <88654509+MathiasBuus@users.noreply.github.com>
2025-02-24 09:06:50 +0100

    Update aggregate.js
    Update aggregate.js

--- browser/helpers/aggregate.js
+++ browser/helpers/aggregate.js
@@ -2,9 +2,24 @@ import _ from "lodash";
 import moment from "moment";
 
 const func_map = {
-  mean: _.meanBy,
   sum: _.sumBy,
+  mean: _.meanBy,
 };
+
+function calculateWeightedAverage(items) {
+  let totalWeight = 0;
+  let weightedSum = 0;
+
+  for (let j = 1; j < items.length; j++) {
+    const prev = items[j - 1];
+    const curr = items[j];
+    const weight = moment(curr.x).diff(moment(prev.x), "seconds");
+    totalWeight += weight;
+    weightedSum += ((prev.y + curr.y) * weight) / 2;
+  }
+
+  return totalWeight > 0 ? weightedSum / totalWeight : null;
+}
 
 function aggregate(x, y, window, func) {
   const grouped = _(
@@ -17,13 +32,15 @@ function aggregate(x, y, window, func) {
   )
     .groupBy("x")
     .mapValues((item) => {
-      return func_map[func](item, "y");
+      if (func === "mean") {
+        return calculateWeightedAverage(item);
+        // return func_map[func](item, "y");
+      } else {
+        return func_map[func](item, "y");
+      }
     })
     .value();
 
-  var start = moment([2020, 1, 1]);
-  var end = moment([2020, 1, 1]);
-  var width = start.diff(end.add(1, window));
   const entries = Object.entries(grouped);
   if (func === "mean") {
     return {
@@ -31,6 +48,9 @@ function aggregate(x, y, window, func) {
       y: entries.map((elem) => elem[1]),
     };
   } else {
+    var start = moment([2020, 1, 1]);
+    var end = moment([2020, 1, 1]);
+    var width = start.diff(end.add(1, window));
     return {
       x: entries.map((elem) =>
         moment(elem[0])
